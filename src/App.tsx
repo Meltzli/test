1074ece4f2e0a40b4ec7b4d6b0008762260e5bc8
Meltzli <zxforvk@mail.ru>
2024-10-03 23:13:52 +0300

    final version

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,25 @@
-import React from "react";
+import { useState } from "react";
 import { EachElement } from "./Components/EachElement/EachElement";
-export const App = () => (
-  <div>
-    <EachElement
-      name={"Выручка, руб"}
-      currentDay={4000}
-      yesterday={2000}
-      thisDayWeek={3000}
-    />
-    <EachElement
-      name={"Наличные"}
-      currentDay={300000}
-      yesterday={300000}
-      thisDayWeek={300000}
-    />
-    <EachElement
-      name={"Безналичный рассчет"}
-      currentDay={100000}
-      yesterday={100000}
-      thisDayWeek={100000}
-    />
-    <EachElement
-      name={"Кредитные карты"}
-      currentDay={105251}
-      yesterday={105251}
-      thisDayWeek={105251}
-    />
-    <EachElement
-      name={"Средний чек, руб"}
-      currentDay={1300}
-      yesterday={900}
-      thisDayWeek={900}
-    />
-    <EachElement
-      name={"Средний гость, руб"}
-      currentDay={1200}
-      yesterday={800}
-      thisDayWeek={800}
-    />
-    <EachElement
-      name={"Удаления из чека (после оплаты), руб"}
-      currentDay={1000}
-      yesterday={1100}
-      thisDayWeek={900}
-    />
-    <EachElement
-      name={"Удаления из чека (до оплаты), руб"}
-      currentDay={1300}
-      yesterday={1300}
-      thisDayWeek={900}
-    />
-    <EachElement
-      name={"Количество чеков"}
-      currentDay={34}
-      yesterday={36}
-      thisDayWeek={34}
-    />
-    <EachElement
-      name={"Количество гостей"}
-      currentDay={34}
-      yesterday={36}
-      thisDayWeek={32}
-    />
-  </div>
-);
+import data from "./data.json";
+
+export const App = () => {
+  const [openKey, setOpenKey] = useState("");
+
+  return (
+    <div>
+      {Object.entries(data).map(([key, value]) => (
+        <EachElement
+          key={key}
+          name={value.name}
+          currentDay={value.currentDay}
+          yesterday={value.yesterday}
+          thisDayWeek={value.thisDayWeek}
+          onClick={() => setOpenKey(key)}
+          isActive={openKey === key}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default App;
--- src/Components/EachElement/EachElement.tsx
+++ src/Components/EachElement/EachElement.tsx
@@ -1,17 +1,30 @@
-import React from "react";
 import css from "./EachElement.module.css";
-import { Dashboard } from "../Dashboard/Dashboard";
-import { useState } from "react";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
+
+const data = [
+  { name: "1", uv: 400 },
+  { name: "2", uv: 500 },
+  { name: "3", uv: 900 },
+  { name: "4", uv: 700 },
+  { name: "5", uv: 100 },
+  { name: "6", uv: 500 },
+  { name: "7", uv: 900 },
+];
+
 export const EachElement = ({
   name,
   currentDay,
   yesterday,
   thisDayWeek,
+  onClick,
+  isActive,
 }: {
   name: string;
   currentDay: number;
   yesterday: number;
   thisDayWeek: number;
+  onClick: () => void;
+  isActive: boolean;
 }) => {
   const higherValue = Math.max(currentDay, yesterday);
   function color() {
@@ -34,30 +47,41 @@ export const EachElement = ({
     }
   }
 
-  const [open, setOpen] = useState(false);
   let percent = Math.round(((currentDay - yesterday) / yesterday) * 100);
+
   return (
-    <div className={css.container}>
-      <div className={css.nameField}>
-        <span className={css.name}>{name}</span>
+    <div className={css.wrapper}>
+      <div className={css.row} onClick={onClick}>
+        <div className={css.nameField}>
+          <span className={css.name}>{name}</span>
+        </div>
+        <div className={css.currentDayField}>
+          <span className={css.currentDay}>{currentDay}</span>
+        </div>
+        <div className={color()}>
+          <span className={css.yesterday}>{yesterday}</span>
+          <span className={percentColor()}>{percent}%</span>
+        </div>
+        <div
+          className={
+            currentDay >= yesterday
+              ? css.thisDayWeekFieldGreen
+              : css.thisDayWeekField
+          }
+        >
+          <span className={css.thisDayWeek}>{thisDayWeek}</span>
+        </div>
       </div>
-      <div className={css.currentDayField}>
-        <span className={css.currentDay}>{currentDay}</span>
+      <div className={css.graph}>
+        {isActive && (
+          <LineChart width={600} height={300} data={data}>
+            <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="name" />
+            <YAxis />
+          </LineChart>
+        )}
       </div>
-      <div className={color()}>
-        <span className={css.yesterday}>{yesterday}</span>
-        <span className={percentColor()}>{percent}%</span>
-      </div>
-      <div
-        className={
-          currentDay >= yesterday
-            ? css.thisDayWeekFieldGreen
-            : css.thisDayWeekField
-        }
-      >
-        <span className={css.thisDayWeek}>{thisDayWeek}</span>
-      </div>
-      <div>{/* dropdown content - dashboard */}</div>
     </div>
   );
 };
